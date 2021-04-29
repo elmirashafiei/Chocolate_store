298e0069aed8df5877dd7882c040df68c88c289a
Elmira Shafiei <elmirashafiei1365@gmail.com>
2021-04-29 16:30:48 +0300

    feature (acc-models): Created UserAccount and Author models, and added them to the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserAccount, Author
 
 # Register your models here.
+admin.site.register(UserAccount)
+admin.site.register(Author)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=256)
+    ZIP_code = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    country = models.CharField(max_length=128)
+    avatar = models.ImageField()
+
+    ROLES_CHOICES = (
+        ("ADM", "Admin"),
+        ("USR", "User")
+    )
+    role = models.CharField(
+        max_length=3,
+        choices=ROLES_CHOICES,
+        default="USR",
+    )
+
+    COMM_CHOICES = (
+        ("MAIL", "post mail"),
+        ("EMAIL", "email")
+    )
+    preferred_communication_channel = models.CharField(
+        max_length=5,
+        choices=COMM_CHOICES,
+        default="EMAIL",
+    )
+
+    def __str__(self):
+        if self.user.first_name:
+            return self.user.first_name
+        return self.user.username
+
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
