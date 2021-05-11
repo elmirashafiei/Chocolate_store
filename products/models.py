3418090f743e02a0130598a954ed283b0becac60
Elmira Shafiei <elmirashafiei1365@gmail.com>
2021-05-12 00:55:49 +0300

    feature/Pro-Models: modified Category and Product models

--- products/models.py
+++ products/models.py
@@ -1,26 +1,32 @@
 from enum import Enum
 from django.db import models
-from django.db.models import IntegerField
+from django.db.models import  DecimalField
 from accounts.models import Author
 # The connection is bad
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=200, unique=True)
+
     # parent categories and 'children'categories(tree placement)
 
-    def __str__(self):
-        return f'{self.name}'
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     title = models.CharField(max_length=128)
-    description = models.CharField(max_length=256)
+    description = models.TextField(max_length=256)
     #thumbnail =
     
     category = models.ForeignKey(Category, on_delete=models.CASCADE, name="products_under_category")
-    price = IntegerField()
+    price = DecimalField(max_digits=10,decimal_places=2)
     
     PRODUCT_TYPES_CHOICES = (
         ("SUBS", "Subscription to a service"),
