e5af60810a7276d6e646e312b41d9176399dbe1d
Elmira Shafiei <elmirashafiei1365@gmail.com>
2021-05-11 18:28:30 +0300

    feature/Pro-View:modified the product view

--- Chocolate_store/urls.py
+++ Chocolate_store/urls.py
@@ -3,17 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('products.urls')),
     path('orders/', include('orders.urls')),
-    path('product/detail/<pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
 
 # This is for development only, not for production
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products import views
 
+
 app_name = "products"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('products/', views.ProductsViewList.as_view(), name="products")
+    path('products/', views.ProductsViewList.as_view(), name="products"),
+    path('product/detail/<pk>/', views.ProductDetailView.as_view(), name='product_detail')
 ]
